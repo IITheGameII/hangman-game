545e8cd5fe97d9ad1b0e681078171a514afd49ab
Dominik <hdominik96@hotmail.com>
2023-06-27 14:33:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+import Hangman from "./Hangman";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Hangman />
+    </div>
+  );
+}
+
